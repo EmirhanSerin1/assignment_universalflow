f088df28ae13faa137601f554123bf59175377ca
EmirhanSerin1 <emirhanserin.1@gmail.com>
2022-05-21 01:54:59 +0200

    (Feature)=>Firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:assignment_universalflow/firebase_options.dart';
 import 'package:assignment_universalflow/view/home/home.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
