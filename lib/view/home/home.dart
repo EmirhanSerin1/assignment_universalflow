dda26168c98f5b914879da43f391ffe53aae5cb6
EmirhanSerin1 <emirhanserin.1@gmail.com>
2022-05-20 17:22:26 +0200

    (Feature)=> Searh bar part designed

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -1,59 +1,4 @@
-// import 'package:flutter/material.dart';
-
-// class MyHomePage extends StatelessWidget {
-//   const MyHomePage({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: SafeArea(
-//         child: Container(
-//           color: Colors.white,
-//           height: MediaQuery.of(context).size.height,
-//           child: Column(
-//             children: [
-//               Container(
-//                 color: Colors.red,
-//                 height: 70,
-//               ),
-//               Container(
-//                 color: Colors.yellow,
-//                 height: 50,
-//               ),
-//               SizedBox(
-//                 height: MediaQuery.of(context).size.height - 144,
-//                 child: getMenu(),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-
-//   getMenu() {
-//     var dateBefore = 1;
-//     var dateAfter = 2;
-
-//     if (dateAfter > dateBefore) {
-//       return ListView.builder(
-//         itemCount: 5,
-//         itemBuilder: (context, index) {
-//           return Padding(
-//             padding: const EdgeInsets.all(8.0),
-//             child: Container(
-//               height: 60,
-//               color: Colors.blue,
-//             ),
-//           );
-//         },
-//       );
-//     }
-//   }
-// }
-
 import 'dart:math';
-import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -84,25 +29,24 @@ List<Transaction> transactions = List.generate(20, (index) {
 })
   ..sort((v1, v2) => v2.createdMillis - v1.createdMillis);
 
-class TransactionPage extends StatefulWidget {
+class HomePage extends StatefulWidget {
   @override
-  _TransactionPageState createState() => _TransactionPageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _TransactionPageState extends State<TransactionPage> {
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
-        
       ),
       body: buildListView(),
     );
   }
 
-   buildListView() {
+  buildListView() {
     String? prevDay;
     String today = DateFormat("EEE, MMM d, y").format(DateTime.now());
     String yesterday = DateFormat("EEE, MMM d, y")
@@ -112,25 +56,69 @@ class _TransactionPageState extends State<TransactionPage> {
       height: MediaQuery.of(context).size.height,
       child: Column(
         children: [
-          Container(
-            height: 50,
-            color: Colors.red,
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 8, 8, 8),
+            child: Container(
+              height: 30,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Transactions",
+                        style: Theme.of(context)
+                            .textTheme
+                            .subtitle2!
+                            .copyWith(fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(width: 10),
+                      InkWell(onTap: () {}, child: Icon(Icons.search))
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "Cashflow this week: ",
+                        style: Theme.of(context).textTheme.subtitle2!.copyWith(
+                            fontWeight: FontWeight.w500, fontSize: 12),
+                      ),
+                      Text(
+                        "Good ",
+                        style: Theme.of(context).textTheme.subtitle2!.copyWith(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 12,
+                            color: Colors.green),
+                      ),
+                      Text(
+                        "-£250",
+                        style: Theme.of(context).textTheme.subtitle2!.copyWith(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 12),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
           ),
           Expanded(
             child: ListView.builder(
               itemCount: transactions.length,
               itemBuilder: (context, index) {
                 Transaction transaction = transactions[index];
-                DateTime date =
-                    DateTime.fromMillisecondsSinceEpoch(transaction.createdMillis);
+                DateTime date = DateTime.fromMillisecondsSinceEpoch(
+                    transaction.createdMillis);
                 String dateString = DateFormat("EEE, MMM d, y").format(date);
-          
+
                 if (today == dateString) {
                   dateString = "Today";
                 } else if (yesterday == dateString) {
                   dateString = "Yesteday";
                 }
-          
+
                 bool showHeader = prevDay != dateString;
                 prevDay = dateString;
                 return Column(
@@ -138,10 +126,14 @@ class _TransactionPageState extends State<TransactionPage> {
                   children: <Widget>[
                     showHeader
                         ? Container(
-                            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 16, vertical: 16),
                             child: Text(
                               dateString,
-                              style: Theme.of(context).textTheme.subtitle2!.copyWith(
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .subtitle2!
+                                  .copyWith(
                                     color: Theme.of(context).primaryColor,
                                     fontWeight: FontWeight.bold,
                                   ),
@@ -173,9 +165,8 @@ class _TransactionPageState extends State<TransactionPage> {
                 padding: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
                 height: 60,
                 decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black, width: 1),
-                borderRadius: BorderRadius.circular(30)
-                ),
+                    border: Border.all(color: Colors.black, width: 1),
+                    borderRadius: BorderRadius.circular(30)),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
@@ -184,24 +175,39 @@ class _TransactionPageState extends State<TransactionPage> {
                         Container(
                           height: 40,
                           width: 40,
-                          decoration:  BoxDecoration(
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.8),
-                                offset: const Offset(0, 3),
-                                blurRadius: 2,
-                              )
-                            ],
+                          decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.8),
+                                  offset: const Offset(0, 3),
+                                  blurRadius: 2,
+                                )
+                              ],
                               shape: BoxShape.circle,
-                              image:const DecorationImage(
+                              image: const DecorationImage(
                                   image: NetworkImage(
                                       "https://randomuser.me/api/portraits/women/44.jpg"),
                                   fit: BoxFit.cover)),
                         ),
+                        const SizedBox(width: 15),
                         Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text("starbuck"),
-                            Text("12:46, Feb 15 2021")
+                            Text(
+                              "Starbucks",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .subtitle1!
+                                  .copyWith(fontWeight: FontWeight.w600),
+                            ),
+                            Text(
+                              "12:46, Feb 15 2021",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .caption!
+                                  .copyWith(fontSize: 10),
+                            )
                           ],
                         )
                       ],
@@ -211,19 +217,19 @@ class _TransactionPageState extends State<TransactionPage> {
                       crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
                         Container(
-                          
                           child: Text(
                             NumberFormat(" £###,###,###")
                                 .format(transaction.point),
                             style: Theme.of(context)
-                            .textTheme
-                            .subtitle2!
-                            .copyWith(
-                              color: Colors.red.shade700
-                            ),
+                                .textTheme
+                                .subtitle2!
+                                .copyWith(color: Colors.red.shade700),
                           ),
                         ),
-                        Text("**** **** **** **34", style: TextStyle(fontSize: 8),)
+                        Text(
+                          "**** **** **** **34",
+                          style: TextStyle(fontSize: 8),
+                        )
                       ],
                     ),
                   ],
