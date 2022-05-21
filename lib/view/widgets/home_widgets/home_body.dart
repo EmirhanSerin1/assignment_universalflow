7e80b5164a2fbe6d9e03c1a6409df672fd01691a
EmirhanSerin1 <emirhanserin.1@gmail.com>
2022-05-21 02:29:51 +0200

    (Feature)=>Stream added.

--- lib/view/widgets/home_widgets/home_body.dart
+++ lib/view/widgets/home_widgets/home_body.dart
@@ -1,7 +1,6 @@
-import 'dart:math';
-
 import 'package:assignment_universalflow/model/transaction_model.dart';
 import 'package:assignment_universalflow/view/widgets/home_widgets/transaction_details.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -63,74 +62,90 @@ class HomeBody extends StatelessWidget {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              itemCount: trans.names.length,
-              itemBuilder: (context, index) {
-                bool showHeader;
-                double totalExc = 0;
-
-                if (index == 0) {
-                  totalExc = trans.amounts[0] + trans.amounts[1];
-                  showHeader = true;
-                } else if (index + 1 < trans.dates.length && index != 0) {
-                  if (DateFormat("d").format(trans.dates[index]) ==
-                      DateFormat("d").format(trans.dates[index - 1])) {
-                    showHeader = false;
-                  } else {
-                    totalExc = trans.amounts[2] +
-                        trans.amounts[3] +
-                        trans.amounts[4] +
-                        trans.amounts[5];
-                    showHeader = true;
-                  }
+            child: StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection("transactions")
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
                 } else {
-                  showHeader = false;
+                  return ListView.builder(
+                    itemCount: trans.names.length,
+                    itemBuilder: (context, index) {
+                      bool showHeader;
+                      double totalExc = 0;
+
+                      DocumentSnapshot doc = snapshot.data!.docs[index];
+                      
+
+
+                      if (index == 0) {
+                        totalExc = trans.amounts[0] + trans.amounts[1];
+                        showHeader = true;
+                      } else if (index + 1 < trans.dates.length && index != 0) {
+                        if (DateFormat("d").format(trans.dates[index]) ==
+                            DateFormat("d").format(trans.dates[index - 1])) {
+                          showHeader = false;
+                        } else {
+                          totalExc = trans.amounts[2] +
+                              trans.amounts[3] +
+                              trans.amounts[4] +
+                              trans.amounts[5];
+                          showHeader = true;
+                        }
+                      } else {
+                        showHeader = false;
+                      }
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          showHeader
+                              ? Container(
+                                  width: MediaQuery.of(context).size.width,
+                                  padding: const EdgeInsets.only(
+                                      left: 16, top: 12, right: 16),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        DateFormat('d.MM.yyyy')
+                                            .format(trans.dates[index]),
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .subtitle2!
+                                            .copyWith(
+                                              color: Colors.black54,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                      ),
+                                      Text(
+                                        (-totalExc).toString(),
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .subtitle2!
+                                            .copyWith(
+                                              color: Colors.black54,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              : const Offstage(),
+                          TransactionDetails(
+                            amount: (-trans.amounts[index]),
+                            cart: trans.carts[index],
+                            date: trans.dates[index],
+                            imageUrl: trans.images[index],
+                            name: trans.names[index],
+                          ),
+                        ],
+                      );
+                    },
+                  );
                 }
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    showHeader
-                        ? Container(
-                            width: MediaQuery.of(context).size.width,
-                            padding: const EdgeInsets.only(
-                                left: 16, top: 12, right: 16),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  DateFormat('d.MM.yyyy')
-                                      .format(trans.dates[index]),
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .subtitle2!
-                                      .copyWith(
-                                        color: Colors.black54,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                ),
-                                Text(
-                                  (-totalExc).toString(),
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .subtitle2!
-                                      .copyWith(
-                                        color: Colors.black54,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                ),
-                              ],
-                            ),
-                          )
-                        : const Offstage(),
-                    TransactionDetails(
-                      amount: (-trans.amounts[index]),
-                      cart: trans.carts[index],
-                      date: trans.dates[index],
-                      imageUrl: trans.images[index],
-                      name: trans.names[index],
-                    ),
-                  ],
-                );
               },
             ),
           ),
@@ -139,7 +154,6 @@ class HomeBody extends StatelessWidget {
     );
   }
 
-  
   getAllSum(Trans trans) {
     var sum = 0.0;
     for (double n in trans.amounts) {
